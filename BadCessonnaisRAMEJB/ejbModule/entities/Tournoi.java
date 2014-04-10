94c2927840cf077ececd03a307a174b9dcc28644
emmanuel.robert <emmanuel.robert@201214-05.internet.etrs>
2014-04-10 14:55:44 +0200

    Tournoi 2eme version

--- BadCessonnaisRAMEJB/ejbModule/entities/Tournoi.java
+++ BadCessonnaisRAMEJB/ejbModule/entities/Tournoi.java
@@ -3,10 +3,10 @@ package entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -14,7 +14,21 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Version;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
 @Entity
+@Data
+@FieldDefaults(level=AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of={"id"})
+@ToString
 @NamedQueries(
 		{
 	@NamedQuery(name="findAllTournoi", query="SELECT t FROM Tournoi t"),
@@ -29,13 +43,14 @@ public class Tournoi implements Serializable{
 	
 	//champs technique
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	@Column(columnDefinition="VARCHAR(36)")
+	private String id = UUID.randomUUID().toString();
 	@Version
 	private long version;
 
 	
 	//@Pattern(regexp="[A-Za-z]*")
+	@Column(columnDefinition="VARCHAR(40)")
 	private String nom;
 	
 	
@@ -44,56 +59,7 @@ public class Tournoi implements Serializable{
 	
 	
 	
-	public Tournoi(){}
-	
-	
-	
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 
-
-	public Date getDateTournoi() {
-		return dateTournoi;
-	}
-	public void setDateTournoi(Date dateTournoi) {
-		this.dateTournoi = dateTournoi;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Tournoi other = (Tournoi) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
 	
 
 }
