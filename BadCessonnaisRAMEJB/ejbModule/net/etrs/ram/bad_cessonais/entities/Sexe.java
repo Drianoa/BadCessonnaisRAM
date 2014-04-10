abe50f868954ce982ba4b2a187508d26be6c65ae
adrien.merly <adrien.merly@gmail.com>
2014-04-10 15:27:15 +0200

    Enumeration sexe

---
+++ BadCessonnaisRAMEJB/ejbModule/net/etrs/ram/bad_cessonais/entities/Sexe.java
@@ -0,0 +1,21 @@
+package net.etrs.ram.bad_cessonais.entities;
+
+/**
+ * Enumeration des différents sexes disponibles.
+ * @author adrien.merly
+ *
+ */
+public enum Sexe {
+	HOMME("Homme"),
+	FEMME("Femme");
+	
+	private String libelle;
+	
+	private Sexe(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	public String getLibelle() {
+		return libelle;
+	}
+}
